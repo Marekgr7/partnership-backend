f39f8b75a24e66bb3363a2776d2018caa43eca54
Marekgr7 <38496758+Marekgr7@users.noreply.github.com>
2020-07-02 13:20:16 +0200

    Delete PersonalData.js

--- personal/PersonalData.js
+++
@@ -1,6 +0,0 @@
-class PersonalData {
-    static mongoUrl = 'mongodb+srv://Marekgr7:Gryszq@cluster0-bcsy0.mongodb.net/partnership?retryWrites=true&w=majority';
-    static secretJwt = 'secret';
-}
-
-module.exports = PersonalData;
